b8b1678abce2e3be80a5da4e881b7d0352284a9e
balamurugan213 <balainfo21@gmail.com>
2021-11-07 02:01:28 +0530

    next blog

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (  
+        <footer>
+            Created by balamurugan213
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+    return ( 
+    <nav>
+        <div className="logo">
+        <Image src='/hamlet_logo.png' width={128} height={128} alt="logo"/>
+        </div>
+        <Link href="/"><a >Home</a></Link>
+        <Link href="/about"><a>About</a></Link>
+        <Link href="/service"><a>Service</a></Link>
+        <Link href="/users"><a>Users</a></Link>
+
+    </nav> );
+}
+
+export default Navbar;
